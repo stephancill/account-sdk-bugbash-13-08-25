8897ad10e6b03edcfd6a7cd2f6e9899d0bf2cbac
Stephan Cilliers <stephan.cilliers@coinbase.com>
2025-08-13 16:00:13 +0200

    fix: base path

--- examples/testapp/next.config.mjs
+++ examples/testapp/next.config.mjs
@@ -1,6 +1,6 @@
 export default {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/account-sdk' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? '/account-sdk-bugbash-13-08-25' : undefined,
   pageExtensions: ['page.tsx', 'page.ts', 'page.js', 'page.jsx'],
   eslint: {
     // Ignore eslint for `next lint`.
